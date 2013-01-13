45282de77b28a315880d8f0d3e91aada32799237
Nathan Lasseter <nathan@4574.co.uk>
2013-01-13 22:31:48 +0000

    Hack to support nginx

--- viewpost.php
+++ viewpost.php
@@ -4,6 +4,7 @@
 		exit(0);
 	}
 	require("settings.php");
+	$getpost = preg_replace('/\+/', ' ', $_GET['post']);
 ?>
 
 <!DOCTYPE html>
@@ -24,11 +25,11 @@
 </div>
 
 <?php
-	$file = $_GET['post'];
+	$file = $getpost;
 	echo "<div>\n";
 	echo "<a class=\"title\" href=\"{$blogRoot}post/" . urlencode($file) . "\">$file <span id=\"perma\">[Permalink]</span></a> ";
 ?>
-<a href="https://twitter.com/share" class="twitter-share-button" data-text="<?php echo "$blogTitle: {$_GET['post']}"; ?>">[Tweet]</a>
+<a href="https://twitter.com/share" class="twitter-share-button" data-text="<?php echo "$blogTitle: {$getpost}"; ?>">[Tweet]</a>
 <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
 
 <?php
