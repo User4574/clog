d73ac31a1f39091a54d0e463794ce52b4f36b17f
Daniel Bailey <danieljabailey@gmail.com>
2013-05-17 18:15:21 +0100

    Added TinyMCE WYSIWYG editor to the post.php page, settings.php contains options for enabling or disabling WYSIWYG

--- post.php
+++ post.php
@@ -7,6 +7,46 @@
 <head>
 	<title><?php echo $blogTitle; ?></title>
 	<link rel="stylesheet" type="text/css" href="<?php echo $blogRoot; ?>styles.css" />
+	<?php
+		if (($WYSIWYG=='miniMCE')
+		||($WYSIWYG=='fullMCE')){
+			$buttons = "";
+			if($WYSIWYG == 'miniMCE'){
+				$buttons = "selector: 'textarea',
+				plugins: [
+					'advlist autolink lists link image charmap print preview anchor',
+					'searchreplace visualblocks code fullscreen',
+					'insertdatetime media table contextmenu paste'
+				],
+				toolbar: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image'";
+			}
+			if($WYSIWYG == 'fullMCE'){
+				$buttons = "selector: 'textarea',
+				theme: 'modern',
+				plugins: [
+					'advlist autolink lists link image charmap print preview hr anchor pagebreak',
+					'searchreplace wordcount visualblocks visualchars code fullscreen',
+					'insertdatetime media nonbreaking save table contextmenu directionality',
+					'emoticons template paste'
+				],
+				toolbar1: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
+				toolbar2: 'print preview media | forecolor backcolor emoticons',
+				image_advtab: true,
+				templates: [
+					{title: 'Test template 1', content: 'Test 1'},
+					{title: 'Test template 2', content: 'Test 2'}
+				],";
+			}
+		
+			echo "
+			<script type='text/javascript' src='$blogRoot" . "tinymce/js/tinymce/tinymce.min.js'></script>
+			<script type='text/javascript'>
+			tinymce.init({
+				$buttons
+			});
+			</script>";
+		}
+	?>
 </head>
 
 <body class='clog_body'>
