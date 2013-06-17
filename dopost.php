703739df223b0d647ebeac8bb52bfeb92f023e26
Daniel Bailey <danieljabailey@gmail.com>
2013-06-17 23:17:57 +0100

    fix dopost!

--- dopost.php
+++ dopost.php
@@ -6,23 +6,50 @@
 		exit(0);
 	}
 
-	$file = $_POST['title'];
+	$title = $_POST['title'];
+	$file = time();
 	$content = $_POST['content'];
 	$nameissafe = preg_match('/^[a-z0-9][a-z0-9 ]*$/i', $file);
 
 	if ($nameissafe){
-		file_put_contents("{$blogPosts}$file", $content);
+		file_put_contents("{$blogPosts}$file", $title . "\n" . $content);
 	}
 	else{
 		header("location: {$blogRoot}post.php?e=1");
 		exit(0);
 	}
 	
-	//update the ls list
-	$ls = `ls -1t {$blogPosts}/`;
-	$lsout = fopen(".lsout", "w");
-	fputs($lsout, $ls);
-	fclose($lsout);
+$ls = `ls -1 {$blogPosts}/`;
+$files = explode("\n", trim($ls));
+$filenames = array();
+$intvals = array();
+$titles = array();
+foreach ($files as $file){
+	$f = fopen("$blogPosts" . "$file", 'r');
+	$thisName = trim(fgets($f)); 
+	$titles[] = $thisName;
+	$filenames[] = $file;
+	$intvals[] = intval($file);
+}
+rsort($intvals);
+$newfilenames = array();
+$newtitles = array();
+foreach ($intvals as $val){
+	$index = array_search($val, $filenames);
+	$file = $filenames[$index];
+	$thisName = $titles[$index];
+	$newfilenames[] = $file;
+	$newtitles[] = 	$thisName;
+}
+$title = implode("\n", $newtitles);
+$nameout = fopen(".nmout", "w");
+fputs($nameout, $title);
+fclose($nameout);
+$filen = implode("\n", $newfilenames);
+$lsout = fopen(".lsout", "w");
+fputs($lsout, $filen);
+fclose($lsout);
+
 	
 	//redirect
 	header("location: {$blogRoot}");
