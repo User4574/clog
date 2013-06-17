a7173a5ddc236ed12cec330e72d2d87fe80f5eee
Daniel Bailey <danieljabailey@gmail.com>
2013-06-17 22:34:07 +0100

    fixed widget

--- widget.php
+++ widget.php
@@ -9,8 +9,9 @@
 
 <head>
 	<title>$blogTitle</title>
-	<link rel='stylesheet' type='text/css' href='$blogRoot/styles.css' />
-</head>
+	<link rel='stylesheet' type='text/css' href='$blogRoot/styles.css' />";
+	include("sharescript.php");
+echo"</head>
 <body class='clog_body'>";
 }
 
@@ -27,21 +28,21 @@
 	} else
 		$file = preg_replace('/\+/', ' ', $_GET['post']);
 
+	$post = file_get_contents("{$blogPosts}$file");
+	$title = strtok($post, "\n");
+	$post = preg_replace('/^.+\n/', '', $post);
+	$post = preg_replace('/\n/', "<br>\n", $post);
+
 	echo "<div class='clog_post_div_widget'>\n";
-	echo "<a class='clog_title' href=\"{$blogRoot}viewpost.php?post=" . urlencode($file) . "\">$file <span class='clog_perma'>[Permalink]</span></a> ";
-?>
+	echo "<a class='clog_title' href=\"{$blogRoot}viewpost.php?post=" . urlencode($file) . "\">$title <span class='clog_perma'>[Permalink]</span></a> ";
 
-<a href="https://twitter.com/share" class="twitter-share-button" data-text="<?php echo "$blogTitle: {$file}"; ?>" data-url="http://<?php echo "{$_SERVER['SERVER_NAME']}{$_SERVER['REQUEST_URI']}"; ?>">[Tweet]</a>
-<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+	include("sharethis.php");
 
-<?php
-	$stat = stat("{$blogPosts}$file");
-	$date = date('d-m-Y H:i T', $stat['mtime']);
+	$date = date('d-m-Y H:i T', $file);
 	echo "<span class='clog_date'>$date</span>\n";
 	echo "<br><br>\n";
 
-	$post = file_get_contents("{$blogPosts}$file");
-	$post = preg_replace('/\n/', "<br>\n", $post);
+	
 	echo $post;
 	echo "</div>\n</a>\n\n";
 
