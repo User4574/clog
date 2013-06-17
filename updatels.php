c83114bb2b27b4290527f9fdbd6dfeedfaf6bbe3
Daniel Bailey <danieljabailey@gmail.com>
2013-06-17 21:43:35 +0100

    updatels page now handles metadata

--- updatels.php
+++ updatels.php
@@ -7,11 +7,23 @@
 <body>
 <?php
 require("settings.php");
-$ls = `ls -1t {$blogPosts}/`;
+$ls = `ls -1 {$blogPosts}/`;
 $lsout = fopen(".lsout", "w");
 fputs($lsout, $ls);
 fclose($lsout);
-echo ".lsout file generated"
+echo ".lsout file generated<br>";
+$files = explode("\n", trim($ls));
+$names = array();
+foreach ($files as $file){
+	//echo "$blogPosts" . "$file<br>";
+	$f = fopen("$blogPosts" . "$file", 'r');
+	$names[] = trim(fgets($f));
+}
+$name=implode("\n", $names);
+$nameout = fopen(".nmout", "w");
+fputs($nameout, $name);
+fclose($nameout);
+echo ".nmout file generated<br> All done.";
 ?>	
 </body>
 </html>
